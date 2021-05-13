56075f3c229d99655aecc3f99e0f6c945e6a8c83
Daniel <danielcarvalhovr@hotmail.com>
2021-05-13 11:17:40 -0400

    updated dependencies in Gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@ Book.destroy_all
 User.destroy_all
 marc = User.create(username: "mrusso", email: "mrusso@test.com", password: "password")
 sandi = User.create(username: "Sandi", email: "sandi@test.com", password: "password")
-leadershift = Book.create(name: "Leadershift", author: "John Maxwell", genre: "self-development", user: marc)
-running_with_scissors = Book.create(name: "Running with Scissors", author: "Augusten Burroughs", genre: "memoir", user: sandi)
-you_are_a_badass = Book.create(name: "You are a Badass",author: "Jon Sincero", genre: "self development", user: sandi)
+leadershift = marc.books.create(name: "Leadershift", author: "John Maxwell", genre: "self-development")
+#leadershift = Book.create(name: "Leadershift", author: "John Maxwell", genre: "self-development", user: marc)
+running_with_scissors = sandi.books.create(name: "Running with Scissors", author: "Augusten Burroughs", genre: "memoir")
+#running_with_scissors = Book.create(name: "Running with Scissors", author: "Augusten Burroughs", genre: "memoir", user: sandi)
+you_are_a_badass = sandi.books.create(name: "You are a Badass", author: "Jon Sincero", genre: "self development")
+#you_are_a_badass = Book.create(name: "You are a Badass",author: "Jon Sincero", genre: "self development", user: sandi)
